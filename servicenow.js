7d108be74b5fc29113dd4887d5109f54dddd5b78
Allan Houston <allan.houston@dimensiondata.com>
2015-06-22 16:50:57 +0200

    Changed to Beta repo in links

--- servicenow.js
+++ servicenow.js
@@ -2,21 +2,21 @@
 // @name       MetroConnect ServiceNow Autocomplete (Beta Branch)
 // @namespace  https://github.com/metroconnect/mc_service_now
 // @version    2.0.4
-// @require    https://raw.github.com/metroconnect/mc_service_now/master/jquery.min.js
-// @require    https://raw.github.com/metroconnect/mc_service_now/master/jquery.simulate.js
-// @require    https://raw.github.com/metroconnect/mc_service_now/master/jquery-ui.js
-// @require    https://raw.github.com/metroconnect/mc_service_now/master/dropdown.js
-// @require    https://raw.github.com/metroconnect/mc_service_now/master/actions.js?
-// @require    https://raw.github.com/metroconnect/mc_service_now/master/functions.js
-// @resource   customCSS https://raw.github.com/metroconnect/mc_service_now/master/jquery-ui-1.10.3.custom.css
+// @require    https://raw.github.com/metroconnect/mc_service_now/Beta/jquery.min.js
+// @require    https://raw.github.com/metroconnect/mc_service_now/Beta/jquery.simulate.js
+// @require    https://raw.github.com/metroconnect/mc_service_now/Beta/jquery-ui.js
+// @require    https://raw.github.com/metroconnect/mc_service_now/Beta/dropdown.js
+// @require    https://raw.github.com/metroconnect/mc_service_now/Beta/actions.js?
+// @require    https://raw.github.com/metroconnect/mc_service_now/Beta/functions.js
+// @resource   customCSS https://raw.github.com/metroconnect/mc_service_now/Beta/jquery-ui-1.10.3.custom.css
 // @description MetroConnect ServiceNow Actions
 // @include    https://dimensiondatacygnus.service-now.com/incident.do*
 // @include    https://dimensiondatacygnus.service-now.com/u_request.do*
 // @include    https://dimensiondatacygnus.service-now.com/change_request.do*
 // @include    https://dimensiondatacygnus.service-now.com/task_time_worked.do*
 // @include    https://dimensiondatacygnus.service-now.com/u_reminder.do*
-// @updateURL  https://raw.github.com/metroconnect/mc_service_now/master/metadata.js
-// @downloadURL https://raw.github.com/metroconnect/mc_service_now/master/servicenow.js
+// @updateURL  https://raw.github.com/metroconnect/mc_service_now/Beta/metadata.js
+// @downloadURL https://raw.github.com/metroconnect/mc_service_now/Beta/servicenow.js
 // @copyright  2013, Allan Houston
 // ==/UserScript==
 
