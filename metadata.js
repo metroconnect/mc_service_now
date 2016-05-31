8798ee4a3bd0029764621fdc8dc61a78d8d80018
Allan Houston <allan.houston@dimensiondata.com>
2016-05-31 10:26:05 +0200

    Fixed Metadata

--- metadata.js
+++ metadata.js
@@ -1,26 +1,26 @@
-// ==UserScript==
-// @name       MetroConnect ServiceNow Autocomplete (Beta Branch)
-// @namespace  https://github.com/metroconnect/mc_service_now
-// @version    2.0.6
-// @require    https://raw.github.com/metroconnect/mc_service_now/Beta/jquery.min.js
-// @require    https://raw.github.com/metroconnect/mc_service_now/Beta/jquery.simulate.js
-// @require    https://raw.github.com/metroconnect/mc_service_now/Beta/jquery-ui.js
-// @require    https://raw.github.com/metroconnect/mc_service_now/Beta/dropdown.js
-// @require    https://raw.github.com/metroconnect/mc_service_now/Beta/actions.js?
-// @require    https://raw.github.com/metroconnect/mc_service_now/Beta/functions.js
-// @resource   customCSS https://raw.github.com/metroconnect/mc_service_now/Beta/jquery-ui-1.10.3.custom.css
-// @description MetroConnect ServiceNow Actions
-// @include    https://dimensiondataservices.service-now.com/incident.do*
-// @include    https://dimensiondataservices.service-now.com/u_request.do*
-// @include    https://dimensiondataservices.service-now.com/change_request.do*
-// @include    https://dimensiondataservices.service-now.com/task_time_worked.do*
-// @include    https://dimensiondataservices.service-now.com/u_reminder.do*
-// @updateURL  https://raw.github.com/metroconnect/mc_service_now/Beta/metadata.js
-// @downloadURL https://raw.github.com/metroconnect/mc_service_now/Beta/servicenow.js
-// @grant       GM_getValue
-// @grant       GM_setValue
-// @grant       GM_registerMenuCommand
-// @grant       GM_getResourceText
-// @grant       GM_addStyle
-// @copyright  2016, Allan Houston
-// ==/UserScript==
+//==UserScript==
+//@name       MetroConnect ServiceNow Autocomplete (Beta Branch)
+//@namespace  https://github.com/metroconnect/mc_service_now
+//@version    2.0.6
+//@require    https://raw.github.com/metroconnect/mc_service_now/Beta/jquery.min.js
+//@require    https://raw.github.com/metroconnect/mc_service_now/Beta/jquery.simulate.js
+//@require    https://raw.github.com/metroconnect/mc_service_now/Beta/jquery-ui.js
+//@require    https://raw.github.com/metroconnect/mc_service_now/Beta/dropdown.js
+//@require    https://raw.github.com/metroconnect/mc_service_now/Beta/actions.js?
+//@require    https://raw.github.com/metroconnect/mc_service_now/Beta/functions.js
+//@resource   customCSS https://raw.github.com/metroconnect/mc_service_now/Beta/jquery-ui-1.10.3.custom.css
+//@description MetroConnect ServiceNow Actions
+//@include    https://dimensiondataservices.service-now.com/incident.do*
+//@include    https://dimensiondataservices.service-now.com/u_request.do*
+//@include    https://dimensiondataservices.service-now.com/change_request.do*
+//@include    https://dimensiondataservices.service-now.com/task_time_worked.do*
+//@include    https://dimensiondataservices.service-now.com/u_reminder.do*
+//@updateURL  https://raw.github.com/metroconnect/mc_service_now/Beta/metadata.js
+//@downloadURL https://raw.github.com/metroconnect/mc_service_now/Beta/servicenow.js
+//@grant       GM_getValue
+//@grant       GM_setValue
+//@grant       GM_registerMenuCommand
+//@grant       GM_getResourceText
+//@grant       GM_addStyle
+//@copyright  2016, Allan Houston
+//==/UserScript==
