bc683be63dca06d7efae7938f734ae5e10b741b6
Allan Houston <allan.houston@dimensiondata.com>
2017-01-07 22:20:02 +0200

    Moved back to master branch, starting the MSEN fixes

--- functions.js
+++ functions.js
@@ -61,9 +61,11 @@ function autoClose(incidentRequest,tech_code,tech_regex,resolution_code,resoluti
                                       triggerKeyEventsForString("#sys_display\\.incident\\.u_contract_ci",Array(1).join("\b")+setCI,0,0,simMenu,regexCI);  
                                 });*/
                         }
-                        console.log("Setting CI to",setCI);
-                        $("#sys_display\\.original\\.incident\\.u_contract_ci").val(setCI);
-                        triggerKeyEventsForString("#sys_display\\.incident\\.u_contract_ci",Array(1).join("\b")+setCI,0,0,simMenu,regexCI);
+                        else {
+                                console.log("Setting CI to",setCI);
+                                $("#sys_display\\.original\\.incident\\.u_contract_ci").val(setCI);
+                                triggerKeyEventsForString("#sys_display\\.incident\\.u_contract_ci",Array(1).join("\b")+setCI,0,0,simMenu,regexCI);
+                        }
                 }
                 if (currentClassification != setClassification) {
                         console.log("Setting Classification to",setClassification," regex is",regexClassification);
